ca12ac2944a06c625e96a5b20cceec43bb4bc3a7
Deni Akbar <deny.akbar@kpvendor.id>
2026-01-28 09:51:59 +0700

    feat: add Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,38 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'published_at',
+        'is_draft',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function isDraft()
+    {
+        return is_null($this->published_at);
+    }
+
+    public function isScheduled()
+    {
+        return $this->published_at && $this->published_at > now();
+    }
+
+    public function isPublished()
+    {
+        return $this->published_at && $this->published_at <= now();
+    }
 }
